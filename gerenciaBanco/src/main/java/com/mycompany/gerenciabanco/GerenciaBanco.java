0921c97abca54f59f2f3bf8b15b4e4609e4c7bb2
joao <barrenha95@yahoo.com.br>
2023-09-30 11:23:17 -0300

    criacao_dos_repositorios

---
+++ gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gerenciabanco;
+
+/**
+ *
+ * @author isaba
+ */
+public class GerenciaBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
