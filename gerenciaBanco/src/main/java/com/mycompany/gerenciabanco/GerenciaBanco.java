f415c244d0554a63bac2843f77893c21a247dff3
joao <barrenha95@yahoo.com.br>
2023-10-01 13:49:28 -0300

    coleta_documento

--- gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -1,16 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.gerenciabanco;
 
-/**
- *
- * @author isaba
- */
 public class GerenciaBanco {
 
     public static void main(String[] args) {
-        System.out.println("Ola! Qual seria o seu documento?");
+        System.out.println("Ola! Seja bem vindo ao sistema bancário");
+
+        User usr;
+        usr = new User();
+
+        usr.coletaDados();
+        
     }
 }
