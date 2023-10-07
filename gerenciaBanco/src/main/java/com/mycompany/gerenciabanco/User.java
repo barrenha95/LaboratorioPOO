dcc44a5b0e024f6ee64aa9f228caaed7f0d89241
Joao Victor Isa Barrenha <joao.barrenha@clear.sale>
2023-10-07 07:31:08 -0300

    criacao-bd

--- gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/User.java
+++ gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/User.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class User {
     
-    public char name;
+    public char name; // Declara as variaveis
     public char lastName;
     public long document;
     public int  flagbd;
@@ -13,27 +13,26 @@ public class User {
     public User(){} // Metodo construtor
 
     public long coletaDados(){
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Scanner myObj = new Scanner(System.in);  // Scanner para ler inputs
         System.out.println("Digite o seu documento (apenas numeros)");
 
-        document = myObj.nextLong();  // Read user input
-        System.out.println("Voce digitou o documento: " + document);  // Output user input
+        document = myObj.nextLong();  // Leitura do input do usuario
+        System.out.println("Voce digitou o documento: " + document);
         return document;
     }
     
     public void checaBd(){
-        try
-        {
-            File f = new File("../database.txt");
-            PrintWriter pw = new PrintWriter(new FileOutputStream(f, true));
-            System.out.println("Database localizado com sucesso.");
-        }
-        catch(Exception e)
-        {
-            System.out.println("Database nao localizado.");
-            
-
-        }
+        try{
+            File f = new File("database.txt"); // Indica qual arquivo queremos manipular
+            if(f.createNewFile()){ //Tenta criar um arquivo com o nome dado
+                System.out.println("Database criado!"); //Se nao der erro ele cria
+            }else{
+                System.out.println("Database ja existe!"); //Se arquivo ja existir ele avisa
+            }
+        }catch(IOException e){
+            System.out.println("Ocorreu um erro:");
+            e.printStackTrace(); // Caso falhe, retorna o erro
+        }   
     }
 
 }
